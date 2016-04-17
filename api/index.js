cf76fb98e8996087f5d87cd7b56f03eef5aefd55
Todd Best <toddbest2004@gmail.com>
2016-04-16 23:49:33 -0700

    Added drinks, coffee, shops, outdoors to search. Added simple error handling on server side.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var request = require("request");
 
+var validSearchTypes = ['food','drinks','coffee','shops','outdoors']
+
 /*
 Route: /api/search
 Parameters:
@@ -13,8 +15,14 @@ Parameters:
 	Type of search to perform
 */
 router.get('/search', function(req, res){
+	if(!req.query.ll){
+		return res.status(400).json({error:"Unable to read location."})
+	}
+	if(validSearchTypes.indexOf(req.query.type)===-1){
+		return res.status(400).json({error:"Invalid search type."})
+	}
 	var ll = req.query.ll
-	var type = 'food'
+	var type = req.query.type
 	var url = "https://api.foursquare.com/v2/venues/explore"
 	request({
 		url:url,
@@ -25,7 +33,7 @@ router.get('/search', function(req, res){
 			client_secret:process.env.FOURSQUARE_CLIENT_SECRET,
 			v:20160410,
 			radius: 1000, //radius in meters
-			openNow: 1 //1: only show open, 0: show all
+			openNow: 0 //1: only show open, 0: show all
 		}
 	}, function(error, response, body){
 		var results = JSON.parse(body).response.groups[0].items
