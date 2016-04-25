a9f18a722f6f98c0c0f3fdac7f39bd7522e4eac0
Todd Best <toddbest2004@gmail.com>
2016-04-24 21:08:21 -0700

    Added seatgeek route, added map marker click handler

--- api/index.js
+++ api/index.js
@@ -22,6 +22,7 @@ router.get('/search', function(req, res){
 		return res.status(400).json({error:"Invalid search type."})
 	}
 	var ll = req.query.ll
+	console.log(ll)
 	var type = req.query.type
 	var requestObject = {
 		url:"https://api.foursquare.com/v2/venues/explore",
@@ -40,26 +41,37 @@ router.get('/search', function(req, res){
 
 // 
 
-router.get('/seatgeek', function(req, res){
+router.get('/sg', function(req, res){
+	if(!req.query.ll||!req.query.type){
+		return res.status(400).json({error:"Invalid search type."})
+	}
+	var ll = req.query.ll
+
+	var llSplit = ll.split(',')
+	if(llSplit.length!==2){
+		return res.status(400).json({error:"Invalid search type."})
+	}
+	var lat = parseFloat(llSplit[0])||0
+	var lon = parseFloat(llSplit[1])||0
+	var type = req.query.type
+
+
 	var now = new Date()
 	var dtstart = new Date(now.getTime()+(30*60*1000))
 	var dtend = new Date(now.getTime()+(24*60*60*1000))
 
-	var ll = req.query.ll
-
 	var requestObject = {
 		url: "https://api.seatgeek.com/2/events",
 		qs:{
-			lat: 47.6762633,
-			lon: -122.31862819,
-			type: 'comedy',
+			lat: lat,
+			lon: lon,
+			type: type,
 			range: '10mi',
 			'datetime_utc.gte': dtstart.toISOString(),
 			'datetime_utc.lte': dtend.toISOString()
 		}
 	}
 	seatgeek(requestObject,res)
-	// res.send(dtstart.toISOString())
 })
 
 module.exports = router;
@@ -67,13 +79,36 @@ module.exports = router;
 function foursquare(requestObject, res){
 	request(requestObject, function(error, response, body){
 		var results = JSON.parse(body).response.groups[0].items
-		res.json({count:results.length,results:results})
+		// console.log(results[0])
+		var newResults = results.map(function(result){
+			var ret = {}
+			ret.type="fs"
+			ret.lat = result.venue.location.lat
+			ret.lon = result.venue.location.lng
+			ret.name = result.venue.name
+			ret.hours = result.venue.hours
+			ret.address = result.venue.location.address+' '+result.venue.location.city+' '+result.venue.location.state+' '+result.venue.location.postalCode
+			ret.url = result.venue.url
+			return ret
+		})
+		res.json({count:results.length,results:newResults})
 	})
 }
 
 function seatgeek(requestObject, res){
 	request(requestObject, function(error, response, body){
 		var results = JSON.parse(body)
-		res.json({count:results.length,results:results})
+		var newResults = results.events.map(function(result){
+			var ret = {}
+			ret.type="sg"
+			ret.lat = result.venue.location.lat
+			ret.lon = result.venue.location.lon
+			ret.name = result.venue.name
+			ret.address = result.venue.address+' '+result.venue.extendedAddress
+			ret.url = result.venue.url
+			return ret
+			//no venue image, but some performance images
+		})
+		res.json({count:results.meta.total,results:newResults})
 	})
 }
